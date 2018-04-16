be09bf5cd9e85b75150890a71897c21d742447f0
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-04-16 16:42:33 -0700

    Commit msg

--- pycalc.py
+++ pycalc.py
@@ -7,7 +7,6 @@ def eval_derivative(x, step):
     # Set step at a positive value close to 0
 
     y_a = my_function(x - step)
-
     y_b = my_function(x + step);
 
     m = (y_b - y_a) / ((x + step) - (x - step))
